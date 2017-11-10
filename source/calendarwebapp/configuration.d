524cc11b3fe1c096fbdeca0e276f2717f4e6af34
Johannes Loher <johannes.loher@fg4f.de>
2017-11-10 16:29:19 +0100

    Added possibility to configure the application with a config file or with command line arguments. Currently only selection of the database system and options for those systems are available.

--- source/calendarwebapp/configuration.d
+++ source/calendarwebapp/configuration.d
@@ -3,32 +3,50 @@ module calendarwebapp.configuration;
 import botan.rng.auto_rng : AutoSeededRNG;
 import botan.rng.rng : RandomNumberGenerator;
 
-import calendarwebapp.authenticator : Authenticator, MongoDBAuthenticator,
-    MySQLAuthenticator;
+import calendarwebapp.authenticator : Authenticator;
 import calendarwebapp.calendarwebapp : CalendarWebapp;
-import calendarwebapp.event : EventStore, MongoDBEventStore, MySQLEventStore;
+import calendarwebapp.event : EventStore;
 import calendarwebapp.passhash : BcryptPasswordHasher, PasswordHasher;
-
-import mysql : MySQLPool;
 
 import poodinis;
 
-import vibe.db.mongo.client : MongoClient;
+import vibe.core.log : logInfo;
 import vibe.db.mongo.collection : MongoCollection;
-import vibe.db.mongo.mongo : connectMongoDB;
 
 class Context : ApplicationContext
 {
 public:
     override void registerDependencies(shared(DependencyContainer) container)
     {
-        auto mongoClient = connectMongoDB("localhost");
-        auto pool = new MySQLPool("localhost", "username", "password", "CalendarWebapp");
-        container.register!MySQLPool.existingInstance(pool);
-        container.register!MongoClient.existingInstance(mongoClient);
-        container.register!(EventStore, MySQLEventStore);
-        container.register!(Authenticator, MySQLAuthenticator);
+        container.register!(ValueInjector!Arguments, AppArgumentsInjector);
+        auto arguments = container.resolve!(AppArgumentsInjector).get("");
+        final switch (arguments.database) with (DatabaseArgument)
+        {
+        case mongodb:
+            import vibe.db.mongo.client : MongoClient;
+            import vibe.db.mongo.mongo : connectMongoDB;
+            import calendarwebapp.authenticator : MongoDBAuthenticator;
+            import calendarwebapp.event : MongoDBEventStore;
 
+            auto mongoClient = connectMongoDB(arguments.mongodb.host);
+            container.register!MongoClient.existingInstance(mongoClient);
+            container.register!(EventStore, MongoDBEventStore!());
+            container.register!(Authenticator, MongoDBAuthenticator!());
+            logInfo("Using MongoDB as database system");
+            break;
+        case mysql:
+            import mysql : MySQLPool;
+            import calendarwebapp.authenticator : MySQLAuthenticator;
+            import calendarwebapp.event : MySQLEventStore;
+
+            auto pool = new MySQLPool(arguments.mysql.host, arguments.mysql.username,
+                    arguments.mysql.password, arguments.mysql.database);
+            container.register!MySQLPool.existingInstance(pool);
+            container.register!(EventStore, MySQLEventStore);
+            container.register!(Authenticator, MySQLAuthenticator);
+            logInfo("Using MySQL as database system");
+            break;
+        }
         container.register!(PasswordHasher, BcryptPasswordHasher);
         container.register!(RandomNumberGenerator, AutoSeededRNG);
         container.register!CalendarWebapp;
@@ -41,19 +59,18 @@ class StringInjector : ValueInjector!string
 {
 private:
     string[string] config;
+    @Value() Arguments arguments;
+    bool initialized = false;
 
 public:
-    this() const @safe pure nothrow
-    {
-        // dfmt off
-        config = ["Database name" :           "CalendarWebapp",
-                  "Users collection name":    "users",
-                  "Events collection name" :  "events"];
-        // dfmt on
-    }
 
-    override string get(string key) const @safe pure nothrow
+    override string get(string key) @safe nothrow
     {
+        if (!initialized)
+        {
+            config = ["MongoDB database name" : arguments.mongodb.database,
+                "mysql.table.users" : "users", "mysql.table.events" : "events"];
+        }
         return config[key];
     }
 }
@@ -61,8 +78,10 @@ public:
 class MongoCollectionInjector : ValueInjector!MongoCollection
 {
 private:
+    import vibe.db.mongo.client : MongoClient;
+
     @Autowire MongoClient mongoClient;
-    @Value("Database name")
+    @Value("MongoDB database name")
     string databaseName;
 
 public:
@@ -70,4 +89,64 @@ public:
     {
         return mongoClient.getCollection(databaseName ~ "." ~ key);
     }
+}
+
+class AppArgumentsInjector : ValueInjector!Arguments
+{
+private:
+    Arguments arguments;
+public:
+
+    this()
+    {
+        import vibe.core.args : readOption;
+
+        readOption("database", &arguments.database, "The database system to use.");
+        readOption("mongodb.host", &arguments.mongodb.host,
+                "The host of the MongoDB instance to use.");
+        readOption("mongodb.database", &arguments.mongodb.database,
+                "The name of the MongoDB database to use.");
+        readOption("mysql.host", &arguments.mysql.host, "The host of the MySQL instance to use.");
+        readOption("mysql.username", &arguments.mysql.username,
+                "The username to use for logging into the MySQL instance.");
+        readOption("mysql.password", &arguments.mysql.password,
+                "The password to use for logging into the MySQL instance.");
+        readOption("mysql.database", &arguments.mysql.database,
+                "The name of the MySQL database to use.");
+    }
+
+    override Arguments get(string key) @safe
+    {
+        import std.exception : enforce;
+
+        enforce(key == "", "There is only one instance of Arguments, to inject it use @Value().");
+        return arguments;
+    }
+}
+
+enum DatabaseArgument
+{
+    mongodb,
+    mysql
+}
+
+struct MySQLArguments
+{
+    string host = "localhost";
+    string username = "username";
+    string password = "password";
+    string database = "CalendarWebapp";
+}
+
+struct MongoDBArguments
+{
+    string host = "localhost";
+    string database = "CalendarWebapp";
+}
+
+struct Arguments
+{
+    DatabaseArgument database = DatabaseArgument.mongodb;
+    MySQLArguments mysql;
+    MongoDBArguments mongodb;
 }
