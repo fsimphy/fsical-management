66fa22a4fa96868acb75ce7eaac2c13122946585
Johannes Loher <johannes.loher@fg4f.de>
2018-05-04 09:39:53 +0200

    technical: update mysql-native to 2.2.2

--- travis.sh
+++ travis.sh
@@ -2,14 +2,14 @@
 set -ev
 
 if [ "${BUILD_TYPE}" = "debug" ]; then
-  dub build --override-config vibe-d:tls/openssl --vverbose --compiler=${DC}
+  dub build --override-config vibe-d:tls/openssl --compiler=${DC}
 fi
 
 if [ "${BUILD_TYPE}" = "unittest" ]; then
-  dub test --override-config vibe-d:tls/openssl --vverbose --compiler=${DC} -- -s
+  dub test --override-config vibe-d:tls/openssl --compiler=${DC} -- -s
 fi
 
 if [ "${BUILD_TYPE}" = "unittest-cov" ]; then
-  dub test --coverage --override-config vibe-d:tls/openssl --vverbose --compiler=${DC} -- -s
+  dub test --coverage --override-config vibe-d:tls/openssl --compiler=${DC} -- -s
   bash <(curl -s https://codecov.io/bash)
 fi
