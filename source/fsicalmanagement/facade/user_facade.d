df9b6921907f89cb330a4bacb366c48dbfc73b42
Johannes Loher <johannes.loher@fg4f.de>
2018-08-01 00:17:22 +0200

    chore: add some more documentation

--- source/fsicalmanagement/facade/user_facade.d
+++ source/fsicalmanagement/facade/user_facade.d
@@ -1,5 +1,8 @@
 module fsicalmanagement.facade.user_facade;
 
+/**
+ * Provides functionality to get, create and remove users.
+ */
 class UserFacade
 {
     import fsicalmanagement.business.password_hashing_service : PasswordHashingService;
@@ -13,23 +16,43 @@ private:
     PasswordHashingService passwordHashingService;
 
 public:
+    ///
     this(UserRepository userRepository, PasswordHashingService passwordHashingService)
     {
         this.userRepository = userRepository;
         this.passwordHashingService = passwordHashingService;
     }
 
+    /**
+     * Gets all users.
+     *
+     * Returns: An `InputRange` containing all `User`s.
+     */
     InputRange!User getAllUsers() @safe
     {
         return userRepository.findAll();
     }
 
+    /**
+     * Removes a user.
+     * Params:
+     * id = The id of the user to remove.
+     */
     void removeUserById(const string id) @safe
     {
         userRepository.deleteById(id);
         logInfo("Deleted user with id %s from the database", id);
     }
 
+    /**
+     * Creates a user.
+     * Params:
+     * username = The name of the user.
+     * password = The password of the user .
+     * privilege = The privilege of the user.
+     *
+     * Returns: The created `User`.
+     */
     User createUser(const string username, const string password, const Privilege privilege) @safe
     {
         import vibe.core.concurrency : async;
