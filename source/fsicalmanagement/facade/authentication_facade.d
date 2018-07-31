f7866649337ac881beb6e5906fe86b171ea9e726
Johannes Loher <johannes.loher@fg4f.de>
2018-08-01 01:07:15 +0200

    fix: fix accidentally changed return type of a function

--- source/fsicalmanagement/facade/authentication_facade.d
+++ source/fsicalmanagement/facade/authentication_facade.d
@@ -28,7 +28,7 @@ public:
      *
      * Throws: `Exception` on invalid $(D_PARAM username) or $(D_PARAM password).
      */
-    void authenticate(const string username, const string password) @safe
+    AuthenticationInfo authenticate(const string username, const string password) @safe
     {
         import std.exception : enforce;
 
